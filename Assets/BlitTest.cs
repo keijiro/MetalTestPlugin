e74e7820d21fdcec748581b0faa94dd38870882e
Keijiro Takahashi <keijiro@gmail.com>
2018-04-21 10:49:33 +0900

    general improvement

---
+++ Assets/BlitTest.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.Collections;
+using System;
+using System.Runtime.InteropServices;
+
+public class BlitTest : MonoBehaviour
+{
+    [SerializeField] MeshRenderer _renderer;
+
+    [DllImport ("MetalTestPlugin")]
+    static extern System.IntPtr Plugin_GetBlitFunction();
+
+    struct BlitParams
+    {
+        public IntPtr source;
+        public IntPtr destination;
+        public uint width;
+        public uint height;
+    };
+
+    NativeArray<BlitParams> _params;
+    CommandBuffer _command;
+    RenderTexture _target;
+
+    void Start()
+    {
+        _params = new NativeArray<BlitParams>(new BlitParams[2], Allocator.Persistent);
+        _command = new CommandBuffer();
+    }
+
+    void OnDestroy()
+    {
+        if (_target != null) RenderTexture.ReleaseTemporary(_target);
+
+        _params.Dispose();
+        _command.Dispose();
+    }
+
+    unsafe void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (_target != null)
+        {
+            if (_target.width != source.width || _target.height != source.height)
+            {
+                RenderTexture.ReleaseTemporary(_target);
+                _target = null;
+            }
+        }
+            
+        if (_target == null)
+        {
+            _target = RenderTexture.GetTemporary(source.width, source.height, 24);
+            _target.Create();
+            _renderer.material.mainTexture = _target;
+        }
+
+        // A heisenbug is here; We have to compare the following pointers
+        // to avoid getting an invalid render buffer.
+        var ptsrc = source.GetNativeTexturePtr();
+        var pbsrc = source.colorBuffer.GetNativeRenderBufferPtr();
+        if (ptsrc == pbsrc) return;
+
+        var p = new NativeSlice<BlitParams>(_params, Time.frameCount & 1);
+
+        p[0] = new BlitParams {
+            source = pbsrc,
+            destination = _target.colorBuffer.GetNativeRenderBufferPtr(),
+            width = (uint)source.width,
+            height = (uint)source.height
+        };
+
+        _command.Clear();
+        _command.IssuePluginEventAndData(
+            Plugin_GetBlitFunction(), 0,
+            (System.IntPtr)p.GetUnsafeReadOnlyPtr()
+        );
+
+        Graphics.ExecuteCommandBuffer(_command);
+        Graphics.Blit(source, destination);
+    }
+}
--- Plugin/MetalTestPlugin/PluginEntry.m
+++ Plugin/MetalTestPlugin/PluginEntry.m
@@ -1,27 +1,50 @@
 #import <Foundation/Foundation.h>
-#import "IUnityGraphicsMetal.h"
-#import "IUnityGraphics.h"
 #import <Metal/MTLBlitCommandEncoder.h>
 #import <Metal/MTLCommandBuffer.h>
+#import "IUnityGraphicsMetal.h"
+#import "IUnityGraphics.h"
+
+typedef struct
+{
+    UnityRenderBuffer source;
+    UnityRenderBuffer destination;
+    uint32 width;
+    uint32 height;
+} BlitParams;
 
 static IUnityInterfaces* s_interfaces;
-static IUnityGraphicsMetal* s_metal;
+static IUnityGraphicsMetal* s_graphics;
 
 static void BlitFunction(int eventId, void* data)
 {
-    if (s_metal == NULL)
-        s_metal = UNITY_GET_INTERFACE(s_interfaces, IUnityGraphicsMetal);
-
-    UnityRenderBuffer* buffers = data;
+    if (s_graphics == NULL)
+        s_graphics = UNITY_GET_INTERFACE(s_interfaces, IUnityGraphicsMetal);
     
-    id<MTLTexture> src = s_metal->TextureFromRenderBuffer(buffers[0]);
-    id<MTLTexture> dst = s_metal->TextureFromRenderBuffer(buffers[1]);
-
-    NSLog(@"MetalTestPlugin: %@, %@", src, dst);
+    if (data == NULL) return;
     
-    s_metal->EndCurrentCommandEncoder();
-    id<MTLBlitCommandEncoder> encoder = [s_metal->CurrentCommandBuffer() blitCommandEncoder];
-    [encoder copyFromTexture:src sourceSlice:0 sourceLevel:0 sourceOrigin:MTLOriginMake(0, 0, 0) sourceSize:MTLSizeMake(256, 256, 1) toTexture:dst destinationSlice:0 destinationLevel:0 destinationOrigin:MTLOriginMake(0, 0, 0)];
+    BlitParams* params = data;
+    
+    if (params->source == NULL || params->destination == NULL) return;
+
+    id<MTLTexture> src = s_graphics->TextureFromRenderBuffer(params->source);
+    id<MTLTexture> dst = s_graphics->TextureFromRenderBuffer(params->destination);
+
+    if (src == nil || dst == nil) return;
+    
+    s_graphics->EndCurrentCommandEncoder();
+    
+    id<MTLBlitCommandEncoder> encoder = [s_graphics->CurrentCommandBuffer() blitCommandEncoder];
+
+    [encoder copyFromTexture:src
+                 sourceSlice:0
+                 sourceLevel:0
+                sourceOrigin:MTLOriginMake(0, 0, 0)
+                  sourceSize:MTLSizeMake(params->width, params->height, 1)
+                   toTexture:dst
+            destinationSlice:0
+            destinationLevel:0
+           destinationOrigin:MTLOriginMake(0, 0, 0)];
+
     [encoder endEncoding];
 }
 
